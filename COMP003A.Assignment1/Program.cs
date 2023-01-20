d2f2c0932dfeeabfe05ca729499ee8bb34952b6a
vanes <vanes@LAPTOP-PUFTS4MN>
2023-01-19 23:13:59 -0800

    Add project files.

---
+++ COMP003A.Assignment1/Program.cs
@@ -0,0 +1,120 @@
+﻿// This is a single line comment
+
+/* This is a multi line comment
+ *
+ * Author: Michael Perez Chavira
+ * Course: COMP-003A-L01
+ * Purpose: Assignment 1
+ */
+
+namespace COMP003A.Assignment1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("100 Things About Me");
+            Console.WriteLine("1. My name is Michael Perez Chavira");
+            Console.WriteLine("2. I am 24 years old");
+            Console.WriteLine("3. My favorite hobby is to go fishing in the canals near lemoore");
+            Console.WriteLine("4. I have a large Star Wars collection that is still growing");
+            Console.WriteLine("5. I have a dog that is turning 1 years old next month");
+            Console.WriteLine("6. I have a girlfriend that I've been with for 6 years");
+            Console.WriteLine("7. My favorite color is black");
+            Console.WriteLine("8. I have 6 instruments that I play when I have free time");
+            Console.WriteLine("9. My favorite genre of music is Metal and Rock");
+            Console.WriteLine("10. I've worked in retail for 4 years");
+            Console.WriteLine("11. I like to workout at midnight when im alone at the gym");
+            Console.WriteLine("12. I mainly focus on strength training rather than size");
+            Console.WriteLine("13. I have wrestled for 5 years");
+            Console.WriteLine("14. I stopped wrestling because of burn out and injury");
+            Console.WriteLine("15. I tore a ligament in my left knee and had surgery");
+            Console.WriteLine("16. A small part of my knee is still numb from surgery 3 years ago");
+            Console.WriteLine("17. I am afraid heights and bees");
+            Console.WriteLine("18. I play on the playstation and the nintendo switch");
+            Console.WriteLine("19. My favorite band is Slipknot and Avenged Sevenfold");
+            Console.WriteLine("20. I am still trying to learn how to play Welcome Home by Coheed and Cambria");
+            Console.WriteLine("21. I have one brother, two sisters, and one half brother");
+            Console.WriteLine("22. I am very family oriented");
+            Console.WriteLine("23. My family is big and still growing");
+            Console.WriteLine("24. My favorite game to play on playstation is Dead By Daylight");
+            Console.WriteLine("25. I uased to be in the Hanford Police Explorers program for 4 years");
+            Console.WriteLine("26. I have an AA in Administration of Justice");
+            Console.WriteLine("27. My first career choice was to become an Aerospace Engineer");
+            Console.WriteLine("28. I plan to adopt a great dane in the future when I have my own house");
+            Console.WriteLine("29. I also plan to build my own PC this year");
+            Console.WriteLine("30. I sometimes go to Rosarito in Mexico to fish off of the pier");
+            Console.WriteLine("31. I enjoy catching crabs and clams than fishing");
+            Console.WriteLine("32. I have been to Ensenada, Rosarito, Tijuana, and Mexicali");
+            Console.WriteLine("33. I found out that my grandpa isn't really my grandpa");
+            Console.WriteLine("34. I had a great dane whose name was Chorizo");
+            Console.WriteLine("35. My current dogs name is Bosco");
+            Console.WriteLine("36. My dog is named after the Earth Kings bear from Avatar The Last Airbender show");
+            Console.WriteLine("37. My dog is half German Shepard and half Labrador Retriever");
+            Console.WriteLine("38. Someone tore my ligament from wrestling pratice in highschool");
+            Console.WriteLine("39. My favorite DC character is Batman");
+            Console.WriteLine("40. The movie that I always watch is The Grand Budapest Hotel");
+            Console.WriteLine("41. I love to watch Dave Chapelles stand ups");
+            Console.WriteLine("42. I did a ton of volunteer work while in explorers");
+            Console.WriteLine("43. I was a Corporal in the police explorers before leaving");
+            Console.WriteLine("44. I left the explorers program because of work and school");
+            Console.WriteLine("45. I'm Hispanic but planning to do a 23 and Me to see if I'm anything else");
+            Console.WriteLine("46. I like to spend a lot of my time outdoors");
+            Console.WriteLine("47. My favorite season is spring");
+            Console.WriteLine("48. I like to go to the beach with my girlfriend to relax");
+            Console.WriteLine("49. I've been to a Dodgers game and plan on going again this year");
+            Console.WriteLine("50. My favorite food is ribs and potato salad");
+            Console.WriteLine("51. I'm saving up to go to disneyland next year");
+            Console.WriteLine("52. I have a Darth Vader lightsaber and im going to buy another one in March");
+            Console.WriteLine("53. I collect Lego Star Wars helmets");
+            Console.WriteLine("54. I play my eletric guitar more than the other instruments");
+            Console.WriteLine("55. Battleship is my favorite board game");
+            Console.WriteLine("56. My girlfriend bought me the same pairs of shoes for 3 years");
+            Console.WriteLine("57. My Friend owns a shaved ice stand but still charges me full price");
+            Console.WriteLine("58. I only use Suavecito pomade for my hair");
+            Console.WriteLine("59. I just recently bought a tv for my game room");
+            Console.WriteLine("60. I spoil my dog more than I do on my girlfriend");
+            Console.WriteLine("61. I usually only like to wear basketball shorts and sandals");
+            Console.WriteLine("62. I have dark brown eyes");
+            Console.WriteLine("63. I like to drink mineral water all the time");
+            Console.WriteLine("64. I overthink a lot more than I should");
+            Console.WriteLine("65. I'm into watching crime youtube videos");
+            Console.WriteLine("66. I still have the same friends from elementary");
+            Console.WriteLine("67. I don't li,ke to work in retail and warehouses");
+            Console.WriteLine("68. My favorite holiday is Christmas");
+            Console.WriteLine("69. I only take hot showers");
+            Console.WriteLine("70. I barely get any sleep");
+            Console.WriteLine("71. I talk in my sleep often");
+            Console.WriteLine("72. One day I want to go to Joji's concert");
+            Console.WriteLine("73. I have a 4 year old nephew");
+            Console.WriteLine("74. My birthday is June 12");
+            Console.WriteLine("75. My favorite preworkout is by Gorilla Mind");
+            Console.WriteLine("76. My favorite is having a spoonfull of peanut butter");
+            Console.WriteLine("77. I dont really crave sweet stuff");
+            Console.WriteLine("78. I dont like to have attention on me");
+            Console.WriteLine("79. I dislike the taste of coffee");
+            Console.WriteLine("80. Most people say that I am an introvert");
+            Console.WriteLine("81. I am very open minded");
+            Console.WriteLine("82. I can understand spanish but I cant speak it");
+            Console.WriteLine("83. I used to only speak spanish when I was little but stopped");
+            Console.WriteLine("84. I love to watch Lord of the Rings and Harry Potter");
+            Console.WriteLine("85. I know most of the Star Wars lore in Canon and some in legends");
+            Console.WriteLine("86. My ncikname is Gordo because I was a chubby baby");
+            Console.WriteLine("87. My favorite ice cream is cookies and cream but I'm also lactose intolerant");
+            Console.WriteLine("89. My coworkers used to call me Deebo");
+            Console.WriteLine("90. I used to workout in layers of clothes to try and drop weight for wrestling");
+            Console.WriteLine("91. I am the tallest person in my family");
+            Console.WriteLine("92. I wanted to play baseball but my parents made me play soccer when I was little");
+            Console.WriteLine("93. I have a hard time smelling things");
+            Console.WriteLine("94. My favorite Star Wars character is Darth Vader because of his story");
+            Console.WriteLine("95. I have a bad habit of biting my nails when im anxious");
+            Console.WriteLine("96. I get scared of alien documentaries");
+            Console.WriteLine("97. My favorite song is Hail to the King by Avenged Sevenfold");
+            Console.WriteLine("98. My favorite gym song is 115 by Elena Siegman");
+            Console.WriteLine("99. I want to have two kids and maybe adopt one");
+            Console.WriteLine("100. My favorite Marvel character is Ghost Rider");
+
+
+        }
+    }
+}
